9bf50c5d6712925aae11ddcf21800c045d9921ab
Hossam <codingWorld5000@gmail.com>
2025-09-23 21:03:29 +0300

    just some organization on the js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,22 @@
+// import databases
 import floor0 from "../database/floor-0.js";
 import floor1 from "../database/floor-1.js";
 import floor2 from "../database/floor-2.js";
 import floor3 from "../database/floor-3.js";
 import floor4 from "../database/floor-4.js";
 
+// import other js files
 import { createFloors } from "./utilities/createFloors.js";
 import { loadSavedData } from "./utilities/loadSavedData.js";
 import { createSidebarLinks } from "./sidebar/sidebar.js";
 import { utilities } from "./utilities/utilities.js";
 
+// cons & vars
 const container = document.querySelector(".container");
 const dateEle = document.getElementById("date");
 const floors = [floor0, floor1, floor2, floor3, floor4];
 
+// events
 window.addEventListener("load", () => {
     const date = new Date().toLocaleDateString("en-UK");
     dateEle.textContent = date;
